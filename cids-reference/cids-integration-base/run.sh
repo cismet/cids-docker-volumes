3480d0317031c818bfec2f733b93819ff29d6a13
Pascal Dihé <pascal@cismet.de>
2016-04-13 14:28:04 +0200

    run scripts updated

--- cids-reference/cids-integration-base/run.sh
+++ cids-reference/cids-integration-base/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -d -p 5434:5432 --name cids-reference-cids-integration-base -v cids-reference:/data  -v /w/git_work/docker-volumes/cids-reference/cids-integration-base/import/:/import/cids-integration-base/ cismet/cids-integration-base:latest
+dockercmd=$(<run.bat)
+eval $dockercmd
--- cids-reference/cids-server/run.sh
+++ cids-reference/cids-server/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -d -p 5434:5432 -p 9986:9986 --name cids-reference-cids-server -e CIDS_ACCOUNT_EXTENSION=CidsReference -e LOG4J_HOST=172.17.0.3 -v cids-reference:/data -v /w/git_work/docker-volumes/cids-reference/cids-server/import/:/import/cids-server/ cismet/cids-server:latest
+dockercmd=$(<run.bat)
+eval $dockercmd
