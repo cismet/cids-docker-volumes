c8dbc80fa2430dfdd00d18b8edc7e9be018c0021
Pascal Dihé <pascal@cismet.de>
2016-04-13 11:13:03 +0200

    integration-base volume mount simplified

--- cids-reference/cids-integration-base/run.sh
+++ cids-reference/cids-integration-base/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 5434:5432 --name cids-reference-cids-integration-base -v cids-reference:/data  -v /w/git_work/docker-volumes/cids-reference/cids-integration-base/import/cids_reference.sql:/import/cids-integration-base/cids_reference.sql cismet/cids-integration-base:latest
+docker run -d -p 5434:5432 --name cids-reference-cids-integration-base -v cids-reference:/data  -v /w/git_work/docker-volumes/cids-reference/cids-integration-base/import/:/import/cids-integration-base/ cismet/cids-integration-base:latest
