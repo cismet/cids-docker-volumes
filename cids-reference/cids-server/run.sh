9ba4b16d8a091209abd8f0a2a07f8989c8744071
Pascal Dihé <pascal@cismet.de>
2016-04-13 11:14:24 +0200

    cids-server volume configuration improved

--- cids-reference/cids-server/run.sh
+++ cids-reference/cids-server/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 9986:9986 --name cids-reference-cids-server -e CIDS_ACCOUNT_EXTENSION=CidsReference -v cids-reference:/data -v /w/git_work/docker-volumes/cids-reference/cids-server/import/settings.xml:/import/cids-server/settings.xml -v /w/git_work/docker-volumes/cids-reference/cids-server/import/pom.xml:/import/cids-server/pom.xml -v /w/git_work/docker-volumes/cids-reference/cids-server/import/runtime.properties:/import/cids-server/runtime.properties.xml cismet/cids-server:latest
+docker run -d -p 5434:5432 -p 9986:9986 --name cids-reference-cids-server -e CIDS_ACCOUNT_EXTENSION=CidsReference -e LOG4J_HOST=172.17.0.3 -v cids-reference:/data -v /w/git_work/docker-volumes/cids-reference/cids-server/import/:/import/cids-server/ cismet/cids-server:latest
