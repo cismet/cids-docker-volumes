9a6617ad7c5dbe0ad55541d04a265bb9b8b49f8d
Pascal Dihé <pascal.dihe@cismet.de>
2016-04-18 14:36:19 +0200

    run scripts updated

--- cids-reference/run.sh
+++ cids-reference/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+docker-compose rm -v -f cids-reference-cids-integration-base
+docker-compose rm -v -f cids-reference-cids-server
+docker-compose rm -v -f cids-reference-cids-server-rest-legacy
 docker rm -v -f cids-reference-cids-integration-base_1
 docker rm -v -f cids-reference-cids-server_1
 docker rm -v -f cids-reference-cids-server-rest-legacy_1
