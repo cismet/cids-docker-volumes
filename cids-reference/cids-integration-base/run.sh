18e7af094dab5bdf0d5d5fb52c0f240648b8abee
p-a-s-c-a-l <pascal@cismet.de>
2016-11-18 16:16:57 +0100

    #7 upgrade to postgres 9.6.1

--- cids-reference/cids-integration-base/run.sh
+++ cids-reference/cids-integration-base/run.sh
@@ -4,4 +4,4 @@ docker stop cids-reference-cids-integration-base
 
 docker rm -v -f cids-reference-cids-integration-base
 docker volume rm cids-reference-cids-integration-base
-docker run -d -p 5434:5432 --name cids-reference-cids-integration-base -v cids-reference-cids-integration-base:/data/ -v ~/docker-volumes/cids-reference/cids-integration-base/import/:/import/cids-integration-base/ cismet/cids-integration-base:postgres-9.0.3
+docker run -d -p 5434:5432 --name cids-reference-cids-integration-base -v cids-reference-cids-integration-base:/data/ -v ~/docker-volumes/cids-reference/cids-integration-base/import/:/import/cids-integration-base/ cismet/cids-integration-base:postgres-9.6.1
